0057e04abc3690509f8db8fa05eee72ba314bfaa
adamijan <a.damijan@t-online.de>
2025-03-19 10:40:31 -0400

    BlockScoping

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -17,8 +17,8 @@ console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
 
 //Block Scope
-console.log(blockVar);
-console.log(blockLet);
+//console.log(blockVar);
+//console.log(blockLet);
 
 function show(){
 var functionVar = "I'm a block-scoped var";
